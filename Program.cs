17a19d66b9b2ca31bafcb3b8c7c2759e6804d683
igor <igor.pravdin163@gmail.com>
2022-10-12 21:26:14 +0400

    1 версия решения

--- Program.cs
+++ Program.cs
@@ -3,3 +3,22 @@
 string[] b = new string[] {"1234", "1567", "-2", "computer scince"};
 
 string[] c = new string[] {"Russia", "Denmark", "Kazan"};
+
+
+void PrintElMin3(string[] x)
+{
+    foreach (string i in x)
+    {
+        if (i.Length <=3)
+        {
+            System.Console.Write(i+" ");
+        }
+    }
+}
+
+
+PrintElMin3(a);
+Console.WriteLine();
+PrintElMin3(b);
+Console.WriteLine();
+PrintElMin3(c);
