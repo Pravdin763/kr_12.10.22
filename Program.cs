41a8dcfc1579a0739956122d0be1af4ec877d937
igor <igor.pravdin163@gmail.com>
2022-10-12 20:57:28 +0400

    добавил массивы

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string[] a = new string[] {"hello", "2", "world", ":-)"};
+
+string[] b = new string[] {"1234", "1567", "-2", "computer scince"};
+
+string[] c = new string[] {"Russia", "Denmark", "Kazan"};
